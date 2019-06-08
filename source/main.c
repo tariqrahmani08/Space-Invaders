e2631a0477c679312762a23c7bae5e5f8e62f282
DanielContreras <contrerass.daniel@gmail.com>
2019-06-08 16:46:27 -0600

    Finished SNES controlle driver with mini program to prompt for input

--- source/main.c
+++ source/main.c
@@ -15,16 +15,17 @@ void write_gpio(unsigned int *gpio, int pin_number, int n);
 int read_gpio(unsigned int *gpio, int pin_number);
 
 int main()
-{
+{   
+    printf("Created by: Tariq Rahmani, Daniel Contreras, Benjamin Sterling\n");
     unsigned int *gpio = gpioPtr();
     int buttons[16] = {};
-    
+    int oldButtons[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
     init_gpio(gpio, CLK, 0b001);
     init_gpio(gpio, LAT, 0b001);
     init_gpio(gpio, DAT, 0b000);    
-
+    printf("Please press a button..\n");
     while (1) 
-    {
+    {   
         write_gpio(gpio, CLK, 1);
         write_gpio(gpio, LAT, 1);
         delayMicroseconds(12);
@@ -40,16 +41,77 @@ int main()
             write_gpio(gpio, CLK, 1);
             i++;
         }
-
-        int k = 0;
-        while (k < 16)
+        
+        
+        if(buttons[3] == 0)
         {
-            printf("%d", buttons[k]);
-            k++;
+            printf("Program is terminating\n");
+            return 0;
         }
-        printf("\n");
+        else if(buttons[0] == 0 && oldButtons[0] == 1)
+        {
+            printf("You have pressed B\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[1] == 0 && oldButtons[1] == 1)
+        {
+            printf("You have pressed Y\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[2] == 0 && oldButtons[2] == 1)
+        {
+            printf("You have pressed Select\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[4] == 0 && oldButtons[4] == 1)
+        {
+            printf("You have pressed Joy-pad UP\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[5] == 0 && oldButtons[5] == 1)
+        {
+            printf("You have pressed Joy-pad DOWN\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[6] == 0 && oldButtons[6] == 1)
+        {
+            printf("You have pressed Joy-pad LEFT\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[7] == 0 && oldButtons[7] == 1)
+        {
+            printf("You have pressed Joy-pad RIGHT\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[8] == 0 && oldButtons[8] == 1)
+        {
+            printf("You have pressed A\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[9] == 0 && oldButtons[9] == 1)
+        {
+            printf("You have pressed X\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[10] == 0 && oldButtons[10] == 1)
+        {
+            printf("You have pressed LEFT\n");
+            printf("Please press a button..\n");
+        }
+        else if(buttons[11] == 0 && oldButtons[11] == 1)
+        {
+            printf("You have pressed RIGHT\n");
+            printf("Please press a button..\n");
+        }
+        else{}
+        
+        int k;
+        for(k=0; k<16; k++)
+        {
+            oldButtons[k] = buttons[k];
+        }
+        delay(50);
     }
-
     return 0;
 }
 
